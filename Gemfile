867226c6d5c50978eed4ae582e66a187653d7a0e
Jordano Moscoso <jor_notes@outlook.com>
2018-09-21 08:44:54 -0500

    First Commit

---
+++ Gemfile
@@ -0,0 +1,34 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '2.5.0'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '~> 5.2.1'
+gem 'pg', '>= 0.18', '< 2.0'
+gem 'puma', '~> 3.11'
+gem 'bootsnap', '>= 1.1.0', require: false
+gem 'rack-cors'
+gem 'kaminari', '~> 1.1', '>= 1.1.1'
+
+group :development, :test do
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 3.8'
+  gem 'factory_bot_rails', '~> 4.11', '>= 4.11.1'
+  gem 'faker', '~> 1.9', '>= 1.9.1'
+  gem 'dotenv-rails', '~> 2.5'
+end
+
+group :development do
+  gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :test do
+  gem 'rails-controller-testing', '~> 1.0', '>= 1.0.2'
+  gem 'shoulda-matchers', '~> 3.1', '>= 3.1.2'
+end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_09_20_024155) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "currencies", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "sign", null: false
+    t.decimal "rate", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "doc", null: false
+    t.string "names", null: false
+    t.string "last_names", null: false
+    t.datetime "birth_date", null: false
+    t.datetime "admission_date", default: "2018-09-20 14:54:11", null: false
+    t.datetime "exit_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movements", force: :cascade do |t|
+    t.datetime "date", null: false
+    t.string "code", null: false
+    t.string "description"
+    t.string "type"
+    t.decimal "amount", null: false
+    t.decimal "rate", null: false
+    t.bigint "currency_id", null: false
+    t.bigint "employee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["currency_id"], name: "index_movements_on_currency_id"
+    t.index ["employee_id"], name: "index_movements_on_employee_id"
+  end
+
+end
